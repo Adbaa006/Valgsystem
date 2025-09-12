640caa7187370836574c28a288614a1a5f1f493d
Adbaa006 <adbaa007@hotmail.com>
2025-09-12 12:29:23 +0200

    Fortsettelse

--- index.py
+++ index.py
@@ -1,22 +1,42 @@
+import random
+
 partier = ["arbeiderpartiet", "fremskrittspartiet", "høyre", "sosialistisk venstreparti", "senterpartiet", "rødt", "miljøpartiet de grønne", "kristelig folkeparti", "venstre"]
 forkortelse = ["ap", "frp", "h", "sv", "sp", "r", "mdg", "krf", "v"]
 stemmer = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+velgere = list(range(1, 4058875))
 
 def flest_stemmer(stemmer):
-    flest = stemmer[0]
-    for stemme in stemmer:
-        if stemme > flest:
-            flest = stemme
-            return flest
+    stemmer.sort()
+    siste = stemmer.pop()
+    return siste
+
 
 def regne_prosent(stemmer):
     total = sum(stemmer)
     prosent = stemmer[i]
     svar = (prosent / total) * 100
-    round(svar/2)
+    round(svar,2)
     return svar
 
+for i in velgere:
+    parti = random.choice(partier)
+    for i in range(len(partier)):
+        if parti == partier[i]:
+            stemmer[i] += 1
+
+
+print("Dette er resultatet: ")
+for i in range(len(partier)):
+    print(f"{partier[i]}: {stemmer[i]}")
+
+print("Stemmer per parti i prosent")
+for i in range(len(stemmer)):
+    print(f"{regne_prosent(stemmer)}%")
+
+print("Partiet med flest stemmer")
+print(flest_stemmer(stemmer))
+
+"""
 should_continue = True
 
 print("\n    Stortingsvalg    ")
@@ -54,4 +74,4 @@ print(f"Partiet som vant fikk {flest_stemmer(stemmer)} stemmer")
 print("Stemmer per parti i prosent")
 for i in range(len(stemmer)):
     print(regne_prosent(stemmer))
-
+"""
